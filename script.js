93b65f9063bd7d900f69a7a6ffbb85d0c6282aa4
MaskLoki <117012952+MaskLoki@users.noreply.github.com>
2022-10-30 18:57:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("button").addEventListener("click", function() {
+    document.body.style.backgroundImage = "url('rick.gif')"
+    //hide the bttn
+    document.querySelector(".everything").style.display = "none"
+
+    document.getElementById("ricky").play()
+})
